8984a0925181e0c153f2c08a3791f8def12800b7
pratham12312 <112676472+pratham12312@users.noreply.github.com>
2024-06-28 20:34:04 +0530

    Here are the files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
+import About from './Components/About';
+import Contactform from './Components/Contactform';
+
+import React from 'react';
+
+import {
+  BrowserRouter,Route,Routes,
+  Link
+} from "react-router-dom";
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+ 
+
+ <BrowserRouter>
+    <Navbar  />
+      <Routes>
+      <Route path='/' element={<Home />} />
+
+      
+          <Route path="ContactForm.js" element={<Contactform/>} />
+
+
+        <Route path="About.js" element={<About />}></Route>
+      
+        
+     
+      </Routes>
+      
+    </BrowserRouter>
+    
+
+
+
     </div>
   );
 }
